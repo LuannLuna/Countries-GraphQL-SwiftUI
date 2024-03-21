579e924fcf305c43bd1e9821cc0d9b91780db300
luannluna <luann.marques@gmail.com>
2024-03-21 12:37:52 -0300

    created project

---
+++ Countries-GraphQL-SwiftUI/Countries-GraphQL-SwiftUI/Countries_GraphQL_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Countries_GraphQL_SwiftUIApp.swift
+//  Countries-GraphQL-SwiftUI
+//
+//  Created by Luann Luna on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Countries_GraphQL_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
